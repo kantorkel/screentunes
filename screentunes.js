c4c509f57026dcbad09dc38b2c4086ab5a70fc06
Matthias Marx <matthias.marx@tuhh.de>
2015-12-07 18:21:42 +0100

    Screen Tunes AM

--- screentunes.js
+++ screentunes.js
@@ -35,7 +35,32 @@
   }
 
   function bars(spacing, height) {
-    ctx.fillStyle = "rgb(0,0,0)";
+//    ctx.fillStyle = "rgb(0,0,0)";
+
+	var p = document.createElement("canvas")
+	p.width=32;
+	p.height=16;
+	var pctx=p.getContext('2d');
+
+	var x0=36;
+	var x1=-4;
+	var y0=-2;
+	var y1=18;
+	var offset=32;
+
+	pctx.strokeStyle = "#ffffff";
+	pctx.lineWidth=4;
+	pctx.beginPath();
+	pctx.moveTo(x0,y0);
+	pctx.lineTo(x1,y1);
+	pctx.moveTo(x0-offset,y0);
+	pctx.lineTo(x1-offset,y1);
+	pctx.moveTo(x0+offset,y0);
+	pctx.lineTo(x1+offset,y1);
+	pctx.stroke();
+
+	ctx.fillStyle=ctx.createPattern(p,'repeat');
+
     var y = 0;
     var maxy = canvas.height;
     while(y < maxy) {
